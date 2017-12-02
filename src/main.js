dd4136948f1129b51ba60f1f8decb7ddefada827
Valorad <valorad@outlook.com>
2017-12-02 14:15:48 +0800

    + webpack + materialize-css basic setup

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import 'materialize-css/dist/js/materialize.min';
+
+// styles
+
+import 'materialize-css/sass/materialize.scss';
+
+// user style
+import './assets/scss/style.scss';
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "static/js/bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader?sourceMap', "sass-loader"]
+          }
+        )
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new CleanWebpackPlugin(['dist']),
+    // new HtmlWebpackPlugin({
+    //   filename: 'index.html',
+    //   template: "src/layouts/index.html"
+    //   // chunks: [] // to tell what bundles to inject.
+    //   // name configured at entry point using a javascript object {}
+    // }),
+    // new HtmlWebpackPlugin({
+    //   filename: '404.html',
+    //   template: "src/layouts/404.html"
+    // }),
+    // new HtmlWebpackPlugin({
+    //   filename: 'list.html',
+    //   template: "src/layouts/_default/list.html"
+    // }),
+    // new HtmlWebpackPlugin({
+    //   filename: 'single.html',
+    //   template: "src/layouts/_default/single.html"
+    // }),
+    
+    new CopyWebpackPlugin([
+      { 
+        from: path.resolve(__dirname, "./src/archetypes"),
+        to: 'archetypes',
+        ignore: [".gitkeep"]
+       },
+       { 
+        from: path.resolve(__dirname, "./src/layouts"),
+        to: 'layouts',
+        ignore: [".gitkeep"]
+       },
+       { 
+        from: path.resolve(__dirname, "./src/static"),
+        to: 'static',
+        ignore: [".gitkeep"]
+       },
+       { 
+        from: path.resolve(__dirname, "./src/theme.toml"),
+        to: ''
+       }
+    ]),
+
+    new ExtractTextPlugin('./static/css/[name].css'),
+
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    })
+
+    
+  ]
+
+
+
+
+}
