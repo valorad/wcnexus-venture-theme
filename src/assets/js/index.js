4594248d5ab6c341634d9a232ab13088114e47c3
wcxaaa <wcx@xtoneict.com>
2017-12-11 16:35:22 +0800

    * fixed date converted to Invalid Date in single page.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -9,10 +9,18 @@ export default () => {
   const instance = new M.Sidenav(elem, options);
 
 
-  // date transformation
-  let dates = document.querySelectorAll("span.date"); // dates: NodeListOf<Element>
+  // date transformation in <time class=date> tag.
+  let dates = document.querySelectorAll("time.date"); // dates: NodeListOf<Element>
   for (let date of dates) {
-    date.innerHTML = formatDistance(new Date(date.innerHTML), new Date())
+    // date.innerHTML: 2017-12-09 11:00:00 +0800 CST. Only Chrome works well when converting this to a date object,
+    // but this booms in other browsers. So we have to extract core date part out via a regex.
+    let re = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/;
+    let dateCore = re.exec(date.innerHTML)[0];
+    date.innerHTML = formatDistance(new Date(dateCore), new Date())
+    // console.log(date.innerHTML);
+    // console.log(new Date(date.innerHTML));
+    // console.log(new Date());
+    // console.log(formatDistance(new Date(date.innerHTML), new Date()));
   }
   
 };
